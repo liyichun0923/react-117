11c0efce22924ecc8a2a867b0a444d7b64e04531
liyichun0923 <569275210@qq.com>
2019-11-07 11:28:41 +0800

    zuoye

---
+++ precache-manifest.fd7ca84ed6f9e8fac57a1f13cf824396.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fca107e27be4ff463eea8423bb8e72e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf32e3ed8056634849ce",
+    "url": "/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "ff582c92c3921ec6d381",
+    "url": "/static/css/main.ae92eea5.chunk.css"
+  },
+  {
+    "revision": "cf32e3ed8056634849ce",
+    "url": "/static/js/2.73c217be.chunk.js"
+  },
+  {
+    "revision": "ff582c92c3921ec6d381",
+    "url": "/static/js/main.dda5919e.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  },
+  {
+    "revision": "270aa38b04f66a0383181829a75f002e",
+    "url": "/static/media/code.270aa38b.png"
+  },
+  {
+    "revision": "7e270b7a6007e42293851db68826a848",
+    "url": "/static/media/ionichina.7e270b7a.png"
+  },
+  {
+    "revision": "edf98ec5279deeda58cbf7548f67e35f",
+    "url": "/static/media/jiguang.edf98ec5.png"
+  },
+  {
+    "revision": "ade0b558e680202f11a2107aae004d90",
+    "url": "/static/media/offer.ade0b558.png"
+  },
+  {
+    "revision": "e9af30027415cdd5b3c8f615c3560431",
+    "url": "/static/media/tu.e9af3002.png"
+  },
+  {
+    "revision": "d95e874a0104a7294fa667510b346769",
+    "url": "/static/media/tu2.d95e874a.png"
+  },
+  {
+    "revision": "334a91adfc1eec848f989eb59dd6d788",
+    "url": "/static/media/tu3.334a91ad.png"
+  },
+  {
+    "revision": "7574737b9e1a90c7c13206b44537f72e",
+    "url": "/static/media/yunbiaoge.7574737b.png"
+  },
+  {
+    "revision": "9cc3e4719b2010ac351e9be5dca8a203",
+    "url": "/static/media/yunzhihui.9cc3e471.png"
+  }
+]);
